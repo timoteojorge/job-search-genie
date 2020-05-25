41d66d0cf7611067b30f5b4c090a30bdc7294f4f
timoteo-jorge <timoteo.jorge@kinesso.com>
2020-05-24 22:01:07 -0300

    bugfix

--- api/src/main/java/com/timoteo/jobsearch/model/Metadata.java
+++ api/src/main/java/com/timoteo/jobsearch/model/Metadata.java
@@ -1,11 +1,15 @@
 package com.timoteo.jobsearch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Metadata {
         private Integer currentOffset;
